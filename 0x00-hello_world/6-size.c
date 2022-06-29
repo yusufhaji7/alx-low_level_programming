b2899cb4285a7081842d80ab2bb68b37cf36a7af
yusufhaji7 <yusufhadji873@gmail.com>
2022-06-29 13:51:57 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function to start any c program
+ * Return: 0 value
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+		Size of an int: 4 byte(s)\n
+		Size of a long int: 4 byte(s)\n
+		Size of a long long int: 8 byte(s)\n
+		Size of a float: 4 byte(s)");
+	return (0);
+}
