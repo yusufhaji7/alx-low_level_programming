d628847003ca58cf3462b5996cd32da93913bf6b
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-01 10:55:42 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,24 @@
-,.
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	int a = 48;
+	int b = 97;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b < 103)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
