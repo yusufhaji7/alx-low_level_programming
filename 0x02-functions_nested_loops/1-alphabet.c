fae7a0701cb5abade0d6ddd2e454e9b88b698115
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 06:23:53 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,19 @@
  * main - check the code
  *
  * print_alphabet - prints a to z
- * Return: Always 0.
+ *
+ * Return: Always 0
  */
+
 void print_alphabet(void)
 {
 	int a = 97;
+
 	while (a <= 122)
 	{
 		_putchar(a);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
 
 int main(void)
