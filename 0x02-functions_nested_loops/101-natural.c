ee8a00e8385fe16dd78f7db80fcb2b873bcb9ef3
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 14:26:30 -0700

    trial

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n = 0, counter = 0;
+
+	while (n  != 1024)
+	{
+		if (((n % 5) == 0) && ((n % 3) == 0))
+		{
+			counter += n;
+		}
+		n++;
+	}
+	return (0);
+}
