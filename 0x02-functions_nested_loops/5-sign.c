29cd042ed83433d3a9cd0ca88905b2db2a65dc0a
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 07:47:37 -0700

    trial

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_sign - checkes lower case
+ *@n: tested number
+ * Return: return 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+		return ("-, -1");
+	else if (n > 0)
+		return ("+, 1");
+	else
+		return ("0, 0")
+}
