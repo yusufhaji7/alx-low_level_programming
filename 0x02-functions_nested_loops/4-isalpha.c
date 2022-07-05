2bd5a3f0fdd43a3111184f30cf0d5a9dc135aac2
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 07:27:38 -0700

    maybe

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isalpha - checkes lower case
+ *@c: tested letter
+ * Return: return 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 48 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
