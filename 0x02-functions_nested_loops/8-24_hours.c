76986a80de4719a4204b60484b0dd4ae2c118f5a
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 12:01:50 -0700

    trial

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * void jack_bauer - prints minutes of 24 hours
+ * jack_bauer - prints minutes of 24 hours
  *
  * Return: void
  */
 
 void jack_bauer(void)
 {
-	int a = 0
-	
+	int a = 0;
+
 	while (a <= 2)
 	{
 		int b = 0;
@@ -17,11 +17,11 @@ void jack_bauer(void)
 		while (b <= 3)
 		{
 			int c = 0;
-			
+
 			while (c <= 5)
 			{
 				int d = 0;
-				
+
 				while (d <= 9)
 				{
 					_putchar(a + '0');
