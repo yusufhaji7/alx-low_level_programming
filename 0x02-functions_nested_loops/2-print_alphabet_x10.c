cf39222473d20df62dbb117617e493ea00a62767
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 06:45:21 -0700

    maybe

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 97;
+	int b = 1;
+
+	while (b <= 10)
+	{
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		b++;
+		_puchar('\n');
+	}
+}
