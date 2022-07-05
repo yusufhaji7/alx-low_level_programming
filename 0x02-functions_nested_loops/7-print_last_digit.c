82e76500c3316f607edb36b0fd0317f16f2c7e39
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-05 10:35:35 -0700

    trial

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,18 @@
 int print_last_digit(int n)
 {
 	if (n > 0)
+	{
+		_putchar('%d', n % 10);
 		return (n % 10);
+	}
 	else if (n < 0)
+	{
+		_putchar('%d', n % 10);
 		return (-1 * (n % 10));
+	}
 	else
+	{
+		_putchar('%d', n % 10);
 		return (0);
+	}
 }
