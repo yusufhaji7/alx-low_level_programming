151d6959dd9829ccbbd17b19e49efb0eef3a0958
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-07 14:00:30 -0700

    first tria;

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - biggest prime factor of 612852475143
+ *
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	int i = 2;
+	int counter;
+	int big_number;
+
+	while (i <= 612852475143)
+	{
+		int j = 0;
+
+		while (j <= 612852475143)
+		{
+			if ((i % j) == 0)
+				counter += 1;
+			j++;
+		}
+		if (counter == 2)
+			big_number = i;
+	}
+	printf(big_number);
+
+	return (0);
+}
