51a3c60188fa61385bb9f09ee4837a935bc9e465
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-07 05:38:48 -0700

    trial

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
