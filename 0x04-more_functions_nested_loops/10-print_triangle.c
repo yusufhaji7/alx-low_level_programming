b814e27175a576349843a2660d02b35128897d73
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-07 13:39:12 -0700

    finale

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print triangle dependent on the parameter size.
+ * @size : The height of the triangle
+ * Return: Void.
+ */
+void print_triangle(int size)
+{i
+	int i;
+	int x;
+	int y;
+
+	for (i = 0; i < size; i++)
+	{
+		for (y = size - 1 - i; y > 0; y--)
+		{
+			_putchar(' ');
+		}
+		for (x = 0; x <= i; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
