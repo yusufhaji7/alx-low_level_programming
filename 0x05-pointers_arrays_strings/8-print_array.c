6ae48602552d6d1f6514dd7b690de68f9f849881
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-11 11:34:08 -0700

    trial

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array
+ * @a: name of the array
+ * @n: number of arrays
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int a[n];
+	int count = 0;
+
+	while (count < n)
+	{
+		printf("%d,", a[count]);
+		count++;
+	}
+	printf("\n");
+}
