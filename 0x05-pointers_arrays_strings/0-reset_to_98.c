0ad4ed95237b313cab83b2026ffc41c22a3f2ce7
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-11 04:53:12 -0700

    trial

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets to 98
+ *@n: number to be reseted
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int *pointer = n;
+	*pointer = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+project 0 - changing to 98
