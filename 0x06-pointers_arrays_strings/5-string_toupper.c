11778cb5dce1946326db308762ec73e7917be2b6
yusufhaji7 <yusufhadji873@gmail.com>
2022-07-14 11:05:08 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_touppper - changes lower to upper
+ * @dest: accpets string as parameter
+ *
+ * Return: dest
+ */
+
+char *string_toupper(char *dest)
+{
+	int i = 0;
+
+	while (dest[i] != '0')
+	{
+		if (dest[i] >= 'a' && dest[i] <= 'z')
+			dest[i] -= 32;
+	}
+	return (dest);
+}
