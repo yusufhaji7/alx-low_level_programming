3062970e4d7bc4fb41ed60dbc8052f79aa117636
yusufhaji7 <yusufhadji873@gmail.com>
2022-08-11 13:51:52 -0700

    betty correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 
 /**
  * print_list - prints all the elements of a linked list
- * h: pointer to the list_t list to print
+ * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
  */
